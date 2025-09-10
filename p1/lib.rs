4224048ba0b83e84ee954b076c1e2d67530e39a9
Andrew McCall <a.mccall@imprintgroup.co.uk>
2025-09-10 11:35:07 +0100

    Added Testing

--- p1/lib.rs
+++ p1/lib.rs
@@ -1,3 +1,4 @@
+/* p1: Multiples of 3 and 5 */
 pub fn solution(limit: usize) -> usize {
     let mut sum = 0;
     for i in 1..limit {
@@ -6,6 +7,12 @@ pub fn solution(limit: usize) -> usize {
         }
     }
     sum
+}
+
+#[test]
+fn test() {
+    assert_eq!(solution(10), 23);
+    assert_eq!(solution(1000), 233168);
 }
 
 // TODO: Lookup Rust magic numbers/branchless solution
