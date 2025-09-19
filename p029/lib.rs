115c8bb21f0a0f25f6dda4570106c2f3c0ad712a
Andrew McCall <a.mccall@imprintgroup.co.uk>
2025-09-19 10:19:19 +0100

    Problem 29

--- p029/lib.rs
+++ p029/lib.rs
@@ -1,53 +1,71 @@
 /* p29: Distinct Powers */
 pub fn solution(max_base: u64) -> u64 {
-    let mut base = max_base;
-    let exponent = max_base;
+    let mut distinct = std::collections::HashSet::new();
 
-    // Prime Factorization
-    let mut factors_base = Vec::new();
+    for a in 2..=max_base {
+        for b in 2..=max_base {
+            let mut base = a;
+            let exponent = b;
 
-    let mut i = 2;
+            let mut factors = Vec::new();
 
-    while i * i < (base + 1) {
-        let mut count = 0;
-        while base % i == 0 {
-            base /= i;
-            count += 1;
-
-            if base < i {
-                break;
+            let mut i = 2;
+            while i * i <= base {
+                let mut count = 0;
+                while base % i == 0 {
+                    base /= i;
+                    count += 1;
+                }
+                if count > 0 {
+                    factors.push((i, (count as u64) * exponent));
+                }
+                if i == 2 {
+                    i = 3;
+                } else {
+                    i += 2;
+                }
             }
-        }
-        if count > 0 {
-            factors.push((i, count * exponent));
-        }
-        if i == 2 {
-            i = 3
-        } else {
-            i += 2;
+
+            if base > 1 {
+                factors.push((base, exponent));
+            }
+
+            let mut exponent_gcd = 0;
+            for &(_, exp_after) in &factors {
+                exponent_gcd = if exponent_gcd == 0 {
+                    exp_after
+                } else {
+                    gcd(exponent_gcd, exp_after)
+                };
+            }
+
+            let smallest_base = if exponent_gcd == 0 {
+                a
+            } else {
+                let mut sb = 1;
+                for &(p, exp_after) in &factors {
+                    let power = exp_after / exponent_gcd;
+                    for _ in 0..power {
+                        sb *= p;
+                    }
+                }
+                sb
+            };
+
+            distinct.insert((exponent_gcd, smallest_base));
         }
     }
 
-    if base > 0 {
-        factors.push((base, exponent));
-    }
-
-    println!("{:?}", factors);
-
-    0
+    distinct.len() as u64
 }
 
 #[test]
 fn test() {
-    solution(5);
-    solution(100);
-    solution(1234567890);
-    // assert_eq!(solution(5), 15);
-    // assert_eq!(solution(100), 9183);
-    // assert_eq!(solution(1000), 9183);
+    assert_eq!(solution(5), 15);
+    assert_eq!(solution(100), 9183);
 }
 
-// Greatest Common Divisor thing
+// Greatest Common Divisor
 fn gcd(mut a: u64, mut b: u64) -> u64 {
     while b != 0 {
         let t = a % b;
